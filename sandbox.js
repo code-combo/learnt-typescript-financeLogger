09b15001222b12c0f4e8b88eda93de7fd5522ddc
Toluwanimi <rjayjutolu419@gmail.com>
2023-03-15 03:08:55 +0100

    Learnt how arrays and object work in ts

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,34 @@
-var character = "luigi";
-var number = 1987;
-var isSapa = false;
-character = "2000";
-number = 2000;
-isSapa = true;
-console.log(character);
-console.log(number);
-console.log(isSapa);
-var calc = function (diameter) {
-    return diameter * Math.PI;
+// let character = "luigi";
+// let number = 1987;
+// let isSapa = false;
+// character = "2000";
+// number = 2000;
+// isSapa = true;
+// console.log(character);
+// console.log(number);
+// console.log(isSapa);
+// const calc = (diameter: number) => {
+//     return diameter * Math.PI;
+// }
+// console.log(calc(3));
+console.log('OBJECTS AND ARRAYS');
+// tutorial 4 - objects and arrays
+var family = ['seyi', 'pelumi', 'toluwanimi', 'simi'];
+// family.push(21);
+family[4] = "anjola";
+var mixed = ["ken", 4, true, "simi"];
+mixed[1] = "5";
+console.log(mixed);
+var individual = {
+    name: "Toluwanimi",
+    techie: false,
+    age: 25,
+    chicks: ['']
 };
-console.log(calc(3));
+individual = {
+    techie: true,
+    name: 'praise',
+    age: 20,
+    chicks: []
+};
+console.log(individual);
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,48 @@
-let character = "luigi";
+// let character = "luigi";
 
-let number = 1987;
+// let number = 1987;
 
-let isSapa = false;
+// let isSapa = false;
 
-character = "2000";
-number = 2000;
-isSapa = true;
+// character = "2000";
+// number = 2000;
+// isSapa = true;
 
-console.log(character);
-console.log(number);
-console.log(isSapa);
+// console.log(character);
+// console.log(number);
+// console.log(isSapa);
 
-const calc = (diameter: number) => {
-    return diameter * Math.PI;
+// const calc = (diameter: number) => {
+//     return diameter * Math.PI;
+// }
+
+// console.log(calc(3));
+
+console.log('OBJECTS AND ARRAYS')
+// tutorial 4 - objects and arrays
+
+let family = ['seyi', 'pelumi', 'toluwanimi', 'simi'];
+// family.push(21);
+family[4] = "anjola";
+
+const mixed = ["ken", 4, true, "simi"]
+mixed[1] = "5";
+
+console.log(mixed);
+
+let individual = {
+    name: "Toluwanimi",
+    techie: false,
+    age: 25,
+    chicks: ['']
+
 }
 
-console.log(calc(3))
+individual = {
+    techie: true,
+    name: 'praise',
+    age: 20,
+    chicks: []
+}
+
+console.log(individual);
