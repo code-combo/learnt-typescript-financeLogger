055b39671653fda764bb73845ae1e91ac951ce64
Toluwanimi <rjayjutolu419@gmail.com>
2023-03-15 02:20:53 +0100

    Learnt type basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,13 @@
 var character = "luigi";
+var number = 1987;
+var isSapa = false;
+character = "2000";
+number = 2000;
+isSapa = true;
 console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+console.log(number);
+console.log(isSapa);
+var calc = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(calc(3));
--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,19 @@
-const character = "luigi";
-console.log(character);
+let character = "luigi";
 
-const inputs = document.querySelectorAll('input');
-inputs.forEach(input => {
-    console.log(input);
-});
+let number = 1987;
+
+let isSapa = false;
+
+character = "2000";
+number = 2000;
+isSapa = true;
+
+console.log(character);
+console.log(number);
+console.log(isSapa);
+
+const calc = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(calc(3))
