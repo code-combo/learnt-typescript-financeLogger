5f7c5531e53c16ed0a83381d58d782dfc605778c
Toluwanimi <rjayjutolu419@gmail.com>
2023-03-20 14:24:13 +0100

    Learnt interfaces and apllied it with classes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,38 @@
+console.log('INTERFACES'); // tutorial 16
+// interface isPerson {
+//      name: string;
+//      age: number;
+//      speak(a: string): void;
+//      spend(b: number): number
+// }
+// const me: isPerson = {
+//     name: "Toluwanimi",
+//     age: 20,
+//     speak(text: string): void {
+//         console.log(text);
+//     },
+//     spend(amount: number): number {
+//         console.log(`dont spend more ${amount} everyday`);
+//         return amount;
+//     }
+// };
+// const greetPerson = (person: isPerson) => {
+//     console.log("hello", person.name);
+// }
+// greetPerson(me);
+// console.log(me);
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
 const invOne = new Invoice('Tinubu', "rigging the election", 1);
 const invTwo = new Invoice('Sonwoolu', "Scamming us", 1);
+let docOne;
+let docTwo;
+docOne = new Invoice("luigi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 300);
+let doc = [];
+doc.push(docOne);
+doc.push(docTwo);
+console.log(doc);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,49 @@
+console.log('INTERFACES') // tutorial 16
+// interface isPerson {
+//      name: string;
+//      age: number;
+//      speak(a: string): void;
+//      spend(b: number): number
+// }
+
+// const me: isPerson = {
+//     name: "Toluwanimi",
+//     age: 20,
+//     speak(text: string): void {
+//         console.log(text);
+//     },
+//     spend(amount: number): number {
+//         console.log(`dont spend more ${amount} everyday`);
+//         return amount;
+//     }
+// };
+
+// const greetPerson = (person: isPerson) => {
+//     console.log("hello", person.name);
+// }
+
+// greetPerson(me);
+
+// console.log(me);
+
 import { Invoice } from "./classes/Invoice.js"
+import { Payment } from "./classes/Payment.js"
+import { HasFormatter } from "./interfaces/HasFormatter.js"
 
 const invOne = new Invoice('Tinubu', "rigging the election", 1);
 const invTwo = new Invoice('Sonwoolu', "Scamming us", 1);
+
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("luigi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 300);
+
+let doc: HasFormatter[] = [];
+doc.push(docOne);
+doc.push(docTwo);
+
+console.log(doc);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne)
