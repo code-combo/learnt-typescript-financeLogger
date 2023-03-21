0cc6a09413b02f56c501fd64302bddb6c9102e3c
Toluwanimi <rjayjutolu419@gmail.com>
2023-03-21 03:48:27 +0100

    Learnt generics

--- public/app.js
+++ public/app.js
@@ -22,17 +22,18 @@ console.log('INTERFACES'); // tutorial 16
 // greetPerson(me);
 // console.log(me);
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 const invOne = new Invoice('Tinubu', "rigging the election", 1);
 const invTwo = new Invoice('Sonwoolu', "Scamming us", 1);
-let docOne;
-let docTwo;
-docOne = new Invoice("luigi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 300);
-let doc = [];
-doc.push(docOne);
-doc.push(docTwo);
-console.log(doc);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("luigi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 300);
+// let doc: HasFormatter[] = [];
+// doc.push(docOne);
+// doc.push(docTwo);
+// console.log(doc);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
@@ -44,7 +45,44 @@ const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector('ul');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.value);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    // console.log(
+    //     // type.value,
+    //     // toFrom.value,
+    //     // details.value,
+    //     // amount.value
+    //     doc
+    // )
+    const list = new ListTemplate(ul);
+    list.render(doc, type.value, "start");
 });
+//  GGENERICS
+const addUID = (obj) => {
+    const uid = Math.floor(Math.random() * 999);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+const docOne = addUID({ name: 'yoshi', age: 40 });
+// const docTwo = addUID("Alaye!");
+console.log(docOne);
+;
+const docTwo = {
+    uid: 419,
+    resourceName: "Tinubu",
+    data: "A town hall differant from bala blue!"
+};
+const doc3 = {
+    uid: 3,
+    resourceName: "Buhari",
+    data: {
+        garbage: "nigeria, from to bottom"
+    }
+};
--- src/app.ts
+++ src/app.ts
@@ -27,23 +27,24 @@ console.log('INTERFACES') // tutorial 16
 // console.log(me);
 
 import { Invoice } from "./classes/Invoice.js"
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js"
 import { HasFormatter } from "./interfaces/HasFormatter.js"
 
 const invOne = new Invoice('Tinubu', "rigging the election", 1);
 const invTwo = new Invoice('Sonwoolu', "Scamming us", 1);
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-docOne = new Invoice("luigi", "web work", 250);
-docTwo = new Payment("mario", "plumbing work", 300);
+// docOne = new Invoice("luigi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 300);
 
-let doc: HasFormatter[] = [];
-doc.push(docOne);
-doc.push(docTwo);
+// let doc: HasFormatter[] = [];
+// doc.push(docOne);
+// doc.push(docTwo);
 
-console.log(doc);
+// console.log(doc);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne)
@@ -59,13 +60,57 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+const ul = document.querySelector('ul') as HTMLUListElement
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-    console.log(
-        type.value,
-        toFrom.value,
-        details.value,
-        amount.value
-    )
+
+    let doc: HasFormatter
+    if(type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+
+    // console.log(
+    //     // type.value,
+    //     // toFrom.value,
+    //     // details.value,
+    //     // amount.value
+    //     doc
+    // )
+
+    const list = new ListTemplate(ul);
+    list.render(doc, type.value, "start");
 })
+
+//  GGENERICS
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    const uid = Math.floor(Math.random() * 999);
+    return { ...obj, uid};
+}
+
+const docOne = addUID({name: 'yoshi', age: 40});
+// const docTwo = addUID("Alaye!");
+console.log(docOne);
+
+interface Resource<T> {
+    uid: number,
+    resourceName: string,
+    data: T
+};
+
+const docTwo: Resource<String> = {
+    uid: 419,
+    resourceName: "Tinubu",
+    data: "A town hall differant from bala blue!"
+}
+
+const doc3: Resource<object> = {
+    uid: 3,
+    resourceName: "Buhari",
+    data: {
+        garbage: "nigeria, from to bottom"
+    }
+}
